bbec67131c93847d7419bcc81af11fa6a13af0f4
YuriyStetskyi <yuriystetskyi@gmail.com>
2023-12-13 01:07:17 +0200

    created project

---
+++ Source/Shooter_MP/Shooter_MP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Shooter_MP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Shooter_MP, "Shooter_MP" );
---
+++ Source/Shooter_MP/Shooter_MPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Shooter_MPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_MP_API AShooter_MPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
